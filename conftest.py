86e29f26d5bd74f248327c54e65f523a43888749
NazarYakh <nazaryakh@gmail.com>
2021-08-30 12:21:40 +0300

    add file conftest.py

--- conftest.py
+++ conftest.py
@@ -6,7 +6,7 @@ from selenium.webdriver.chrome.options import Options
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default='chrome',
                      help="Choose browser: chrome or firefox")
-    parser.addoption('--language', action='store', default='en-gb',
+    parser.addoption('--language', action='store', default='en',
                      help="Write correct language argument in terminal command (e.g. '--language=uk') and try again")
 
 
