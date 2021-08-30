2553f73684683768175d0769a693c2ec7746e282
NazarYakh <nazaryakh@gmail.com>
2021-08-30 12:23:25 +0300

    updated test_main_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,15 +1,14 @@
-import time
 from .pages.main_page import MainPage
-from pages.basket_page import BasketPage
+from .pages.basket_page import BasketPage
 from .pages.login_page import LoginPage
 import pytest
+import time
+
+link = "http://selenium1py.pythonanywhere.com/"
 
 
-
-@pytest.mark.skip(reason="need_review")
-@pytest.mark.need_review
+@pytest.mark.skip
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
@@ -18,10 +17,8 @@ def test_guest_can_go_to_login_page(browser):
     login_page.should_be_login_page()
 
 
-@pytest.mark.skip(reason="need_review")
-@pytest.mark.need_review
+@pytest.mark.skip
 def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page1 = LoginPage(browser, link)
     page.open()
@@ -31,10 +28,8 @@ def test_guest_should_see_login_link(browser):
     page1.should_be_login_page()
 
 
-@pytest.mark.skip(reason="need_review")
-@pytest.mark.need_review
+@pytest.mark.skip
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
-    link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)
     page.open()
     page.go_to_basket_page()
@@ -44,10 +39,10 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
 
 
 @pytest.mark.login_guest
-class TestLoginFromMainPage():
+class TestLoginFromMainPage:
+    link = "http://selenium1py.pythonanywhere.com/"
 
     def test_guest_can_go_to_login_page(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/"
         page = MainPage(browser, link)
         page.open()
         page.go_to_login_page()
@@ -55,11 +50,6 @@ class TestLoginFromMainPage():
         login_page.should_be_login_page()
 
     def test_guest_should_see_login_link(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/"
         page = MainPage(browser, link)
         page.open()
         page.should_be_login_link()
-
-
-
-
